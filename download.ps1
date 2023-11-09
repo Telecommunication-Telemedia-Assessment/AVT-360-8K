682c03d51900dd4296d8b771168ffabe75956cee
Steve Göring <steve.goering@tu-ilmenau.de>
2023-11-09 14:29:32 +0100

    minor changes

--- download.ps1
+++ download.ps1
@@ -1,9 +1,8 @@
 @echo off
-set "base_url="
+set "base_url=https://avtshare01.rz.tu-ilmenau.de/avt-360-8k/"
 
 powershell -command "& {Invoke-WebRequest -Uri '%base_url%/pvss.zip' -OutFile 'pvss.zip'}"
 powershell -command "& {Expand-Archive -Path 'pvss.zip' -DestinationPath '.'}"
 powershell -command "& {Invoke-WebRequest -Uri '%base_url%/srcs.zip' -OutFile 'srcs.zip'}"
 powershell -command "& {Expand-Archive -Path 'srcs.zip' -DestinationPath '.'}"
-powershell -command "& {Invoke-WebRequest -Uri '%base_url%/test_data.zip' -OutFile 'test_data.zip'}"
-powershell -command "& {Expand-Archive -Path 'test_data.zip' -DestinationPath '.'}"
+
--- download.sh
+++ download.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-base_url="https://resdata.tu-ilmenau.de/public/ei/avt/"
+base_url="https://avtshare01.rz.tu-ilmenau.de/avt-360-8k/"
 
 wget -c "$base_url/pvss.zip"
 unzip pvss.zip
 wget -c "$base_url/srcs.zip"
 unzip srcs.zip
-wget -c "$base_url/test_data.zip"
-unzip test_data.zip
